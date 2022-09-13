ef620a0c3cbb80e607aa165d0d8bfb02aea35055
imayuki-prog <95353284+imayuki-prog@users.noreply.github.com>
2022-09-13 14:39:09 -0700

    t

---
+++ assignment2.1_4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int first;
+    int second;
+    int third;
+    
+    cout << "Enter three interger" << endl;
+    cout << "First number  : "; cin >> first;
+    cout << "Second number : "; cin >> second;
+    cout << "Third number  : "; cin >> third;
+    
+    cout << first  << endl;
+    cout << second << endl;
+    cout << third  << "\n" << endl;
+
+    cout << third  << endl;
+    cout << second << endl;
+    cout << first  << endl;
+
+}
